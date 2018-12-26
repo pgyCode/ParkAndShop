0c17e7fce4c6fe479660d02f95da0217e11cbcd6
asimid <812073053@qq.com>
2018-12-26 17:33:45 +0800

    customer updata

--- src/components/customer/Order.vue
+++ src/components/customer/Order.vue
@@ -13,22 +13,22 @@
       <ul>
         <li v-bind:key="order" v-for="order in orders">
           <div style="height: 42px;background: rgb(241, 241, 241); border-bottom: 1px solid #dfdfdf">
-            <p style="float: left; color: rgb(60, 60, 60); font-size: 12px; width: 100px; text-align: center; font-weight: 800; line-height: 42px ">{{order.time}}</p>
-            <p style="float: left; color: rgb(60, 60, 60); font-size: 12px; width: 130px; text-align: center; font-weight: 400; line-height: 42px ">OrderNum: {{ order.orderNum}}</p>
-            <p style="float: left; color: rgb(60, 60, 60); font-size: 12px; width: 300px; text-align: center; font-weight: 400; line-height: 42px ">{{ order.shopName }}</p>
-            <p style="float: right; color: rgb(60, 60, 60); font-size: 12px; width: 90px; text-align: center; font-weight: 400; line-height: 42px ">{{ order.sellerName }}</p>
+            <p style="float: left; color: rgb(60, 60, 60); font-size: 12px; width: 100px; text-align: center; font-weight: 800; line-height: 42px ">{{order.createTime}}</p>
+            <p style="float: left; color: rgb(60, 60, 60); font-size: 12px; width: 130px; text-align: center; font-weight: 400; line-height: 42px ">OrderNum: {{ order.orderID}}</p>
+            <p style="float: left; color: rgb(60, 60, 60); font-size: 12px; width: 300px; text-align: center; font-weight: 400; line-height: 42px ">{{ order.product.shopName }}</p>
+            <p style="float: right; color: rgb(60, 60, 60); font-size: 12px; width: 90px; text-align: center; font-weight: 400; line-height: 42px ">{{ order.product.shopName }}</p>
           </div>
           <div style="padding: 15px; clear: both;">
-            <img style="float: left; width: 80px; height: 80px; padding-right: 15px" v-bind:src="order.url"/>
+            <img style="float: left; width: 80px; height: 80px; padding-right: 15px" v-bind:src="order.product.portraitURL"/>
             <div style="width: 220px; float: left; height: 80px;">
-              <p style=" color: rgb(60, 60, 60); font-size: 13px; font-weight: 500; line-height: 25px; height: 55px; ">{{ order.goodName }}</p>
+              <p style=" color: rgb(60, 60, 60); font-size: 13px; font-weight: 500; line-height: 25px; height: 55px; ">{{ order.product.pName }}</p>
               <span style="clear:both; width: 100px; text-align: center; display: block; color: #fff; background: #f40; font-size: 12px;padding: 1px 3px; margin-top: 3px">Authenticated</span>
             </div>
-            <p style="float: left; width: 100px;text-align: center; color: rgb(60, 60, 60); font-size: 12px; font-weight: 400; line-height: 80px">¥{{ order.price }}</p>
+            <p style="float: left; width: 100px;text-align: center; color: rgb(60, 60, 60); font-size: 12px; font-weight: 400; line-height: 80px">¥{{ order.product.price }}</p>
             <p style="float: left; width: 100px;text-align: center; color: rgb(60, 60, 60); font-size: 12px; font-weight: 400; line-height: 80px">{{ a = 1 }}</p>
-            <p style="float: left; width: 100px;text-align: center; color: rgb(60, 60, 60); font-size: 12px; font-weight: 600; line-height: 80px">¥{{ order.price }}</p>
+            <p style="float: left; width: 100px;text-align: center; color: rgb(60, 60, 60); font-size: 12px; font-weight: 600; line-height: 80px">¥{{ order.product.price }}</p>
             <p style="float: left; width: 100px;text-align: center; color: rgb(60, 60, 60); font-size: 12px; font-weight: 600; line-height: 80px">Succeed</p>
-            <button style="float: right; width: 100px;text-align: center; color: rgb(60, 60, 60); font-size: 12px; font-weight: 600; height: 26px; width: 60px; border: 1px solid rgb(220, 220, 220); border-radius: 5px; margin-top: 27px; cursor: pointer">Refund</button>
+            <button style="float: right; width: 100px;text-align: center; color: rgb(60, 60, 60); font-size: 12px; font-weight: 600; height: 26px; width: 60px; border: 1px solid rgb(220, 220, 220); border-radius: 5px; margin-top: 27px; cursor: pointer" v-on:click="onClickRefund(order.orderID)">Refund</button>
           </div>
         </li>
       </ul>
@@ -52,65 +52,35 @@ export default {
   },
 
   methods: {
-
+    initLoad: function () {
+      this.isLoad = true
+      this.$http.get(this.URL + 'c/lookupOrders?cID=' + this.getCookie('userId'))
+        .then((data) => {
+          console.log(data)
+          this.isLoad = false
+          const response = data.body
+          this.orders = response.data.orderArr
+        })
+        .catch(() => {
+          this.isLoad = false
+        })
+    },
+    onClickRefund: function (id) {
+      this.isLoad = true
+      this.$http.get(this.URL + 'c/refund?orderID=' + id)
+        .then((data) => {
+          console.log(data)
+          this.isLoad = false
+          this.initLoad()
+        })
+        .catch(() => {
+          this.isLoad = false
+        })
+    }
   },
 
   created: function () {
-    this.isLoad = true
-    this.$http.get(this.URL + 'm/seller_black_info')
-      .then((data) => {
-        this.orders = [
-          {
-            time: '2018-12-19',
-            orderNum: '0',
-            url: 'https://g-search2.alicdn.com/img/bao/uploaded/i4/TB1oAS6qzDpK1RjSZFrXXa78VXa.jpg_240x240Q90.jpg',
-            shopName: 'HuaShuo',
-            sellerName: 'HuaShuo',
-            price: 5999.00,
-            goodName: 'HuaShuo ROG PHONE'
-          },
-          {
-            time: '2018-12-19',
-            orderNum: '1',
-            url: 'https://g-search3.alicdn.com/img/bao/uploaded/i4/TB15ZaPjSzqK1RjSZFpXXakSXXa.jpg_240x240Q90.jpg',
-            shopName: 'Razer',
-            sellerName: 'Razer',
-            price: 5990.99,
-            goodName: 'Razer Phone 2'
-          },
-          {
-            time: '2018-12-19',
-            orderNum: '2',
-            url: 'https://g-search3.alicdn.com/img/bao/uploaded/i4/TB10zbvIFXXXXbKXFXXXXXXXXXX.jpg_240x240Q90.jpg',
-            shopName: 'Nokia',
-            sellerName: 'Nokia',
-            price: 768.00,
-            goodName: 'Nokia Phone 1020'
-          },
-          {
-            time: '2018-12-19',
-            orderNum: '3',
-            url: 'https://g-search3.alicdn.com/img/bao/uploaded/i4/TB1NOxOHVXXXXXOXXXXXXXXXXXX.jpg_240x240Q90.jpg',
-            shopName: 'Nokia',
-            sellerName: 'Nokia',
-            price: 866.00,
-            goodName: 'Nokia Phone 1520'
-          },
-          {
-            time: '2018-12-19',
-            orderNum: '4',
-            url: 'https://g-search2.alicdn.com/img/bao/uploaded/i4/TB1SkfgHFXXXXc8apXXXXXXXXXX.jpg_240x240Q90.jpg',
-            shopName: 'Nokia',
-            sellerName: 'Nokia',
-            price: 598.00,
-            goodName: 'Nokia Phone 930'
-          }
-        ]
-        this.isLoad = false
-      })
-      .catch(() => {
-        this.isLoad = false
-      })
+    this.initLoad()
   }
 }
 </script>
