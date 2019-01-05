5d85d49339266197d7b925b7cc69e98f1d95427b
transZZY <2792512448@qq.com>
2019-01-05 11:40:30 +0800

    除order外的界面优化

--- src/main.js
+++ src/main.js
@@ -51,9 +51,10 @@ Vue.prototype.setHtml = function (key, value) {
   document.getElementById(key).html = value
 }
 
-// Vue.prototype.URL = 'http://47.106.11.120:8080/DiGou/api/'
+Vue.prototype.URL = 'http://47.106.11.120:8080/DiGou/api/'
 // Vue.prototype.URL = 'http://127.0.0.1:8000/api/'
-Vue.prototype.URL = 'http://192.168.31.150:8080/api/'
+// Vue.prototype.URL = 'http://192.168.31.150:8080/api/'
+// Vue.prototype.URL = 'http://192.168.43.41:8080/api/'
 
 /* eslint-disable no-new */
 new Vue({
